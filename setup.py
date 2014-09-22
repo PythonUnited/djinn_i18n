251d9a7592ced9f13d7fa1f2d7434c9c08d91ae5
Duco Dokter <dokter@w20e.com>
2014-09-22 14:03:11 +0200

    prepare 1.0.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = [
     ]
 
 setup(name='djinn_i18n',
-      version="1.0.0",
+      version="1.0.1",
       description='Djinn i18n',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
