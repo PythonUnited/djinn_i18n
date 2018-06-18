5ae964ef821e4ce2f4d725d2735f5b62bd71a743
Martin Bouma <bouma@egoactive.com>
2018-06-18 17:08:19 +0200

    translations reload fixed

--- djinn_i18n/views/reload.py
+++ djinn_i18n/views/reload.py
@@ -2,38 +2,20 @@ from django.contrib import messages
 from djinn_core.views.admin import AdminMixin
 from .index import IndexView
 
+
 class ReloadView(IndexView, AdminMixin):
 
-
     def get(self, request, *args, **kwargs):
-        from django.utils import translation
+
         from django.utils.translation import trans_real
-        try:
-            from threading import local
-        except ImportError:
-            from django.utils._threading_local import local
-
-        _thread_locals = local()
-
-        import gettext
 
         try:
-            # Reset gettext.GNUTranslation cache.
-            gettext._translations = {}
+            messages.add_message(
+                request, messages.SUCCESS, "Translations reloaded")
 
-            # Reset Django by-language translation cache.
-            trans_real._translations = {}
-
-            # Delete Django current language translation cache.
-            trans_real._default = None
-
-            messages.add_message(request, messages.SUCCESS, "Translations reloaded")
-
-            # Delete translation cache for the current thread,
-            # and re-activate the currently selected language (if any)
-            prev = trans_real._active.pop(_thread_locals, None)
-            if prev:
-                translation.activate(prev.language())
+            # this was added in django 1.7 or 1.8. Much better than the
+            # previous threadlocal approach
+            trans_real.reset_cache(setting='LANGUAGES')
 
         except AttributeError as e:
             pass
